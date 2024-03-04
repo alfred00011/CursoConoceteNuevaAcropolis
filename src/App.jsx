c68ae4c74117a0e643dca1d7ed1e4e276144ffaa
lenin <lmora@cmac-cusco.com.pe>
2024-03-03 22:04:40 -0500

    build: initializing project Curso Conocete a ti mismo|NavBar integration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./assets/components/common/NavBar"
+function App() {
+
+  return (
+    <>
+      <div className='bg-slate-900'>
+        <NavBar/>
+        <h2>React AP</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/common/Header.jsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+    return(
+
+        <nav className="nav-bar">
+            <ul>
+                <li><a href ="/">Inicio</a></li>
+                <li><a href ="content">Contenido</a></li>
+                <li><a href ="advantage">Beneficios</a></li>
+                <li><a href ="about">¿Quiénes Somos?</a></li>
+                <li><a href ="info">Contactanos</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Header;
